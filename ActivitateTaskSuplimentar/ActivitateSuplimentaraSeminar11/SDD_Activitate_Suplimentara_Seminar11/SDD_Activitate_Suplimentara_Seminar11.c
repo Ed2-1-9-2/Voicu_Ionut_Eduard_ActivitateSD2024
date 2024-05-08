f9d8b4e6555865a4e1012d68d41d8feb2d9b2330
Edi-V <voicueduard784@gmail.com>
2024-05-08 11:59:24 +0300

    Am completat codul respectand cerintele

--- ActivitateTaskSuplimentar/ActivitateSuplimentaraSeminar11/SDD_Activitate_Suplimentara_Seminar11/SDD_Activitate_Suplimentara_Seminar11.c
+++ ActivitateTaskSuplimentar/ActivitateSuplimentaraSeminar11/SDD_Activitate_Suplimentara_Seminar11/SDD_Activitate_Suplimentara_Seminar11.c
@@ -1,6 +1,19 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_NODES 1000
+/*
+* Cerinta :
+Sa se implementeze un arbore binar de cautare echilibrat.
+
+La inserare se verifica gradul de echilibru, iar in cazul in care exista un dezechilibru sa se relaizeze rotirile necesare, astfel incat arborele sa fie echilibrat.
+
+Sa se realizeze functia de cautare a unui  nod in arborele AVL, pe baza unui id, sau a cheii de cautare din arbore.
+
+Sa se realizeze o functie care salveaza nodurile din arborele AVL intr-un vector, prin parcurgerea preordine, inordine, postordine.(Apel in main :Facut la preordine , imi trebuie postordine si inordine)
+Sa se realizeze o functie care salveaza nodurile din arborele AVL intr-o lista simplu inlantuita, prin parcurgerea preordine, inordine, postordine.(Apel in main :Facut la preordine , imi trebuie postordine si inordine)
+Sa se realizeze o functie care salveaza nodurile din arborele AVL intr-o lista dublu inlantuita, prin parcurgerea preordine, inordine, postordine.(Apel in main :Facut la preordine , imi trebuie postordine si inordine)
+*/
 // Revin cu o analiza amanuntita
 typedef struct Node {
     int key;
@@ -125,6 +138,49 @@ void preOrdine(Node* root, Node** nodes, int* index) {
         preOrdine(root->right, nodes, index);
     }
 }
+void inserareListaDublaDeNod(ListNode** head, ListNode** tail, Node* data) {
+    ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = *tail;
+
+    if (*tail != NULL) {
+        (*tail)->next = newNode;
+    }
+    else {
+        *head = newNode;
+    }
+
+    *tail = newNode;
+}
+void preOrdineLaVector(Node* root, Node** nodes, int* index) {
+    if (root != NULL) {
+        nodes[*index] = root;
+        (*index)++;
+        preOrdineLaVector(root->left, nodes, index);
+        preOrdineLaVector(root->right, nodes, index);
+    }
+}
+
+
+void inOrdineLaVector(Node* root, Node** nodes, int* index) {
+    if (root != NULL) {
+        inOrdineLaVector(root->left, nodes, index);
+        nodes[*index] = root;
+        (*index)++;
+        inOrdineLaVector(root->right, nodes, index);
+    }
+}
+
+
+void postOrdineLaVector(Node* root, Node** nodes, int* index) {
+    if (root != NULL) {
+        postOrdineLaVector(root->left, nodes, index);
+        postOrdineLaVector(root->right, nodes, index);
+        nodes[*index] = root;
+        (*index)++;
+    }
+}
 void inOrdine(Node* root, ListNode** head) {
     if (root != NULL) {
         inOrdine(root->left, head);
@@ -148,6 +204,26 @@ void preOrdineLaListaSimpla(Node* root, ListNode** head) {
         preOrdineLaListaSimpla(root->right, head);
     }
 }
+void inOrdineLaListaDubla(Node* root, ListNode** head, ListNode** tail) {
+    if (root != NULL) {
+        inOrdineLaListaDubla(root->left, head, tail);
+
+        ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+        newNode->data = root;
+        newNode->next = NULL;
+        newNode->prev = *tail;
+
+        if (*tail != NULL)
+            (*tail)->next = newNode;
+        else
+            *head = newNode;
+
+        *tail = newNode;
+
+        inOrdineLaListaDubla(root->right, head, tail);
+    }
+}
+
 void preOrdineLaListaDubla(Node* root, ListNode** head, ListNode** tail) {
     if (root != NULL) {
         ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
@@ -161,6 +237,24 @@ void preOrdineLaListaDubla(Node* root, ListNode** head, ListNode** tail) {
         *tail = newNode;
         preOrdineLaListaDubla(root->left, head, tail);
         preOrdineLaListaDubla(root->right, head, tail);
+    }
+}
+void postOrdineLaListaDubla(Node* root, ListNode** head, ListNode** tail) {
+    if (root != NULL) {
+        postOrdineLaListaDubla(root->left, head, tail);
+        postOrdineLaListaDubla(root->right, head, tail);
+
+        ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+        newNode->data = root;
+        newNode->next = NULL;
+        newNode->prev = *tail;
+
+        if (*tail != NULL)
+            (*tail)->next = newNode;
+        else
+            *head = newNode;
+
+        *tail = newNode;
     }
 }
 
@@ -198,6 +292,11 @@ void freeAVL(Node* root) {
 int main() {
     Node* root = NULL;
 
+    Node* nodes[MAX_NODES];
+    int index = 0;
+    //1.   La inserare se verifica gradul de echilibru, iar in cazul in care exista un dezechilibru sa se relaizeze rotirile necesare, astfel incat arborele sa fie echilibrat.
+
+
     root = insert(root, 10);
     root = insert(root, 20);
     root = insert(root, 30);
@@ -205,12 +304,15 @@ int main() {
     root = insert(root, 50);
     root = insert(root, 25);
 
-    int keyToSearch = 30;
+
+    int keyToSearch = 30; //id-ul cheii
     Node* foundNode = search(root, keyToSearch);
     if (foundNode != NULL)
         printf("Nodul cu cheia %d a fost gasit in arbore.\n", keyToSearch);
     else
         printf("Nodul cu cheia %d nu a fost gasit in arbore.\n", keyToSearch);
+    printf("\n======================================================\n");
+    //2.  Sa se realizeze functia de cautare a unui  nod in arborele AVL, pe baza unui id, sau a cheii de cautare din arbore.
 
     Node* nodesPreOrder[100];
     int indexPreOrder = 0;
@@ -221,28 +323,88 @@ int main() {
     }
     printf("\n");
 
-    ListNode* preOrderList = NULL;
-    preOrdineLaListaSimpla(root, &preOrderList);
-    printf("Nodurile arborelui AVL in lista simplu inlantuita(postordine): ");
-    AfisareListaSimpla(preOrderList);
-    ListNode* inOrderList = NULL;
-    inOrdine(root, &inOrderList);
+    printf("\n======================================================\n");
+    //3. Sa se realizeze o functie care salveaza nodurile din arborele AVL intr-un vector, prin parcurgerea preordine, inordine, postordine.(Apel in main :Facut la preordine , imi trebuie postordine si inordine)
+
+ //Motivul pentru care am incercat sa fac asa este ca nu vreau ca nodurile sa fie copiate in urmatorul tip de ordine
+
+    Node* nodesPreOrdine[MAX_NODES];
+    Node* nodesInOrdine[MAX_NODES];
+    Node* nodesPostOrdine[MAX_NODES];
+
+    int indexPreOrdine = 0;
+    int indexInOrdine = 0;
+    int indexPostOrdine = 0;
+
+    preOrdineLaVector(root, nodesPreOrdine, &indexPreOrdine);
+    inOrdineLaVector(root, nodesInOrdine, &indexInOrdine);
+    postOrdineLaVector(root, nodesPostOrdine, &indexPostOrdine);
+
+    printf("Nodurile din arborele AVL in preordine (stocate in vector): ");
+    for (int i = 0; i < indexPreOrdine; i++) {
+        printf("%d ", nodesPreOrdine[i]->key);
+    }
+    printf("\n");
+
+    printf("Nodurile din arborele AVL in inordine (stocate in vector): ");
+    for (int i = 0; i < indexInOrdine; i++) {
+        printf("%d ", nodesInOrdine[i]->key);
+    }
+    printf("\n");
+
+    printf("Nodurile din arborele AVL in postordine (stocate in vector): ");
+    for (int i = 0; i < indexPostOrdine; i++) {
+        printf("%d ", nodesPostOrdine[i]->key);
+    }
+    printf("\n");
+
+    printf("\n======================================================\n");
+    // 4. Sa se realizeze o functie care salveaza nodurile din arborele AVL intr-o lista simplu inlantuita, prin parcurgerea preordine, inordine, postordine.(Apel in main :Facut la preordine , imi trebuie postordine si inordine)
+
+
+    ListNode* ListapreOrdine = NULL;
+    preOrdineLaListaSimpla(root, &ListapreOrdine);
+    printf("Nodurile arborelui AVL in lista simplu inlantuita(preordine): ");
+    AfisareListaSimpla(ListapreOrdine);
+    ListNode* ListainOrdine = NULL;
+    inOrdine(root, &ListainOrdine);
     printf("Nodurile arborelui AVL in lista simplu inlantuita (inordine): ");
-    AfisareListaSimpla(inOrderList);
+    AfisareListaSimpla(ListainOrdine);
 
-    ListNode* postOrderList = NULL;
-    postOrdine(root, &postOrderList);
+    ListNode* ListapostOrdine = NULL;
+    postOrdine(root, &ListapostOrdine);
     printf("Nodurile arborelui AVL in lista simplu inlantuita (postordine): ");
-    AfisareListaSimpla(postOrderList);
-    ListNode* doubleListHead = NULL;
-    ListNode* doubleListTail = NULL;
-    preOrdineLaListaDubla(root, &doubleListHead, &doubleListTail);
+    AfisareListaSimpla(ListapostOrdine);
+    printf("\n======================================================\n");
+    // 5. Sa se realizeze o functie care salveaza nodurile din arborele AVL intr-o lista dublu inlantuita, prin parcurgerea preordine, inordine, postordine.(Apel in main :Facut la preordine , imi trebuie postordine si inordine)
+
+
+    ListNode* PreOrdineListaDublaCap = NULL;
+    ListNode* PreOrdineListaDublaCoada = NULL;
+    preOrdineLaListaDubla(root, &PreOrdineListaDublaCap, &PreOrdineListaDublaCoada);
     printf("Nodurile arborelui AVL in lista dublu inlantuita(preordine): ");
-    AfisareListaDubla(doubleListHead);
+    AfisareListaDubla(PreOrdineListaDublaCap);
+    ListNode* inOrdineListaDublaCap = NULL;
+    ListNode* inOrdineListaDublaCoada = NULL;
+    inOrdineLaListaDubla(root, &inOrdineListaDublaCap, &inOrdineListaDublaCoada);
+    printf("Nodurile arborelui AVL in lista dublu inlantuita (inordine): ");
+    AfisareListaDubla(inOrdineListaDublaCap);
 
+    ListNode* postOrdineListaDublaCap = NULL;
+    ListNode* postOrdineListaDublaCoada = NULL;
+    postOrdineLaListaDubla(root, &postOrdineListaDublaCap, &postOrdineListaDublaCoada);
+    printf("Nodurile arborelui AVL in lista dublu inlantuita (postordine): ");
+    AfisareListaDubla(postOrdineListaDublaCap);
 
-    // Eliberare memorie
-    freeList(preOrderList);
+    AfisareListaSimpla(ListapostOrdine);
+
+    // Se elibereaza memoria listelor
+    freeList(ListapreOrdine);
+    freeList(ListainOrdine);
+    freeList(ListapostOrdine);
+    freeList(postOrdineListaDublaCap);
+    freeList(PreOrdineListaDublaCap);
+    free(inOrdineListaDublaCap);
     freeAVL(root);
 
     return 0;
